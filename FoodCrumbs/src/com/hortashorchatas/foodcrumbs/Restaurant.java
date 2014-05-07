f9f1428823524a5ffcca9e4b6c751a1b2a148be7
Michael Chang <jhorta13@gmail.com>
2014-05-06 17:34:24 -0700

    added local database into the app

--- FoodCrumbs/src/com/hortashorchatas/foodcrumbs/Restaurant.java
+++ FoodCrumbs/src/com/hortashorchatas/foodcrumbs/Restaurant.java
@@ -7,12 +7,27 @@ public class Restaurant implements java.io.Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 7686178610670313168L;
-	public String business_Id;		// Google Places API id
+	public String business_id;		// Google Places API id
 	public String name;				// Google Places API name
 	public String address;			// Google Places API address
 	public Location location;		// Google Places API location
 	public String rating;			// Google Places API rating
 
+	public Restaurant(String business_id, String name, String address, Location location, String rating) {
+		this.business_id = business_id;
+		this.name = name;
+		this.address = address;
+		this.location = location;
+		this.rating = rating;
+	}
+	
+	public Restaurant() {
+		this.business_id = "";
+		this.name = "";
+		this.address = "";
+		this.location = null;
+		this.rating = "";
+	}
 	/*
 	//public String img_url;		// Google Places API image_url
 	//public String phone_number;	// Google Places API display_phone
